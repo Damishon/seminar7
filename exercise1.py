66c817194e355fb99d31c7dbc2ce24246b640b62
Damishon <dnrbaj@gmail.com>
2021-12-02 13:44:05 +0500

    Seminar7

---
+++ exercise1.py
@@ -0,0 +1,22 @@
+fin=open('brown.txt')
+fout=open('output.txt','w')
+lines=fin.readlines()
+diction={}
+min=None
+for line in lines:
+	words=line.split()
+
+	for i in range(len(words)-2):
+		key=words[i]+' '+words[i+1]
+		if key not in diction:
+			diction[key]=1
+		else:
+			diction[key]+=1
+
+for key in diction:
+	outtxt=key,diction[key]
+	fout.write(str(outtxt))
+	fout.write('\n')
+	if min is None or diction[key]<min:
+			min=diction[key]
+print('minimum:',min)
